5da511752879fbf1f69aaac9d8ee5a6b871794bf
Tetiana Panasiuke <tetiana.panasiuk21@gmail.com>
2022-07-27 08:43:31 +0300

    Updates

---
+++ README.md
@@ -0,0 +1,29 @@
+# Landing page for West Solution
+
+[DESIGN](https://www.figma.com/file/UlBnJHOSYkW4KEPwusZfNQ/Test-task?node-id=1%3A93)
+[DEMO] ()
+
+## Description
+
+I've made responsive layout of the online car store. 
+Main functionality: 
+1. Click on select input, which named 'Model', and choose some car model. After that you'll see the filtered cars cards. Also you can filter cars by a year. 
+2. Subscription form. When you'll enter invalid email you can see text error under the input. Example of a valid email: yourmail@mail.com.
+
+## Technologies that I used:
+
+- HTML5;
+- Bootstrap 5;
+- SCSS;
+- BEM-methodology;
+- JS;
+- Gulp;
+- npm tools;
+
+### To start the project locally you need to enter in terminal such command:
+```
+gulp
+```
+## Dependencies
+- NPM v6.14.16;
+- Node v14.19.1;
---
+++ src/scripts/index.js
@@ -0,0 +1,348 @@
+// Showing menu - mobile and tablet
+
+const hamburger = document.querySelector('.header__burger-btn');
+const menu = document.querySelector('.menu--mobile');
+const closeMenuBtn = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+  menu.style.transform = 'translateX(0)';
+});
+
+closeMenuBtn.addEventListener('click', () => {
+  menu.style.transform = 'translateX(-100%)';
+});
+
+// Showing cards dynamically and filtering cars model when model select
+
+const cars = [
+  {
+    id: 1,
+    model: 'Volkswagen',
+    modelDetails: 'Tiguan',
+    year: 2019,
+    race: '19,855',
+    transmission: 'Automatic',
+    engine: 'Diesel',
+    price: '34,888',
+    photo: '../src/img/cars/Volkswagen/Tiguan.png',
+    tags: ['sold']
+  },
+  {
+    id: 2,
+    model: 'Toyota',
+    modelDetails: 'Highlander XL',
+    year: 2014,
+    race: '69,000',
+    transmission: 'Automatic',
+    engine: 'Gas',
+    price: '27,875',
+    photo: '../src/img/cars/Toyota/highLinder_XL.png',
+    tags: ['new']
+  },
+  {
+    id: 3,
+    model: 'Lexus',
+    modelDetails: 'RX 350 LUXURY',
+    year: 2017,
+    race: '39,850',
+    transmission: 'Automatic',
+    engine: 'Gas',
+    price: '38,775',
+    photo: '../src/img/cars/Lexus/lexus_RX350.png'
+  },
+  {
+    id: 4,
+    model: 'Toyota',
+    modelDetails: 'Highlander LE',
+    year: 2016,
+    race: '104,000',
+    transmission: 'Automatic',
+    engine: 'Gas',
+    price: '23,728',
+    photo: '../src/img/cars/Toyota/highlinder_LE.png'
+  },
+  {
+    id: 5,
+    model: 'Hyundai',
+    modelDetails: 'Elantra',
+    year: 2017,
+    race: '95,000',
+    transmission: 'Automatic',
+    engine: 'Gas',
+    price: '11,788',
+    photo: '../src/img/cars/Hyundai/elantra.png'
+  },
+  {
+    id: 6,
+    model: 'Mercedes-Benz',
+    modelDetails: 'GLE',
+    year: 2016,
+    race: '59,800',
+    transmission: 'Automatic',
+    engine: 'Diesel',
+    price: '38,878',
+    photo: '../src/img/cars/Mercedes/mersedes-benz.png'
+  },
+  {
+    id: 7,
+    model: 'Toyota',
+    modelDetails: 'Venza',
+    year: 2015,
+    race: '99,000',
+    transmission: 'Automatic',
+    engine: 'Gas',
+    price: '18,620',
+    photo: '../src/img/cars/Toyota/venza.png',
+    tags: ['sold']
+  },
+  {
+    id: 8,
+    model: 'Lexus',
+    modelDetails: 'RX 350 F-SPORT',
+    year: 2016,
+    race: '86,000',
+    transmission: 'Automatic',
+    engine: 'Gas',
+    price: '34,888',
+    photo: '../src/img/cars/Lexus/x-sport.png',
+    tags: ['new']
+  },
+  {
+    id: 9,
+    model: 'Volkswagen',
+    modelDetails: 'Tiguan',
+    year: 2019,
+    race: '19,855',
+    transmission: 'Automatic',
+    engine: 'Diesel',
+    price: '34,888',
+    photo: '../src/img/cars/Volkswagen/Tiguan.png'
+  },
+  {
+    id: 10,
+    model: 'Toyota',
+    modelDetails: 'Highlander XL',
+    year: 2014,
+    race: '69,000',
+    transmission: 'Automatic',
+    engine: 'Gas',
+    price: '27,875',
+    photo: '../src/img/cars/Toyota/highLinder_XL.png'
+  },
+  {
+    id: 11,
+    model: 'Lexus',
+    modelDetails: 'RX 350 LUXURY',
+    year: 2017,
+    race: '39,850',
+    transmission: 'Automatic',
+    engine: 'Gas',
+    price: '38,775',
+    photo: '../src/img/cars/Lexus/lexus_RX350.png'
+  },
+  {
+    id: 12,
+    model: 'Toyota',
+    modelDetails: 'Highlander LE',
+    year: 2016,
+    race: '104,000',
+    transmission: 'Automatic',
+    engine: 'Gas',
+    price: '23,728',
+    photo: '../src/img/cars/Toyota/highlinder_LE.png'
+  },
+  {
+    id: 13,
+    model: 'Volkswagen',
+    modelDetails: 'Tiguan',
+    year: 2019,
+    race: '19,855',
+    transmission: 'Automatic',
+    engine: 'Diesel',
+    price: '34,888',
+    photo: '../src/img/cars/Volkswagen/Tiguan.png',
+    tags: ['new']
+  },
+  {
+    id: 14,
+    model: 'Toyota',
+    modelDetails: 'Highlander XL',
+    year: 2014,
+    race: '69,000',
+    transmission: 'Automatic',
+    engine: 'Gas',
+    price: '27,875',
+    photo: '../src/img/cars/Toyota/highLinder_XL.png',
+  },
+  {
+    id: 15,
+    model: 'Lexus',
+    modelDetails: 'RX 350 LUXURY',
+    year: 2017,
+    race: '39,850',
+    transmission: 'Automatic',
+    engine: 'Gas',
+    price: '38,775',
+    photo: '../src/img/cars/Lexus/lexus_RX350.png'
+  },
+  {
+    id: 16,
+    model: 'Toyota',
+    modelDetails: 'Highlander LE',
+    year: 2016,
+    race: '104,000',
+    transmission: 'Automatic',
+    engine: 'Gas',
+    price: '23,728',
+    photo: '../src/img/cars/Toyota/highlinder_LE.png'
+  },
+  {
+    id: 17,
+    model: 'Hyundai',
+    modelDetails: 'Elantra',
+    year: 2017,
+    race: '95,000',
+    transmission: 'Automatic',
+    engine: 'Gas',
+    price: '11,788',
+    photo: '../src/img/cars/Hyundai/elantra.png'
+  },
+  {
+    "id": 18,
+    model: 'Mercedes-Benz',
+    modelDetails: 'GLE',
+    year: 2016,
+    race: '59,800',
+    transmission: 'Automatic',
+    engine: 'Diesel',
+    price: '38,878',
+    photo: '../src/img/cars/Mercedes/mersedes-benz.png'
+  },
+  {
+    id: 19,
+    model: 'Toyota',
+    modelDetails: 'Venza',
+    year: 2015,
+    race: '99,000',
+    transmission: 'Automatic',
+    engine: 'Gas',
+    price: '18,620',
+    photo: '../src/img/cars/Toyota/venza.png'
+  },
+  {
+    id: 20,
+    model: 'Lexus',
+    modelDetails: 'RX 350 F-SPORT',
+    year: 2016,
+    race: '86,000',
+    transmission: 'Automatic',
+    engine: 'Gas',
+    price: '34,888',
+    photo: '../src/img/cars/Lexus/x-sport.png',
+    tags: ['new']
+  },
+  {
+    id: 21,
+    model: 'Volkswagen',
+    modelDetails: 'Tiguan',
+    year: 2019,
+    race: '19,855',
+    transmission: 'Automatic',
+    engine: 'Diesel',
+    price: '34,888',
+    photo: '../src/img/cars/Volkswagen/Tiguan.png',
+    tags: ['new']
+  },
+];
+
+function getCard({model, modelDetails, year, race, transmission, engine, price, photo, tags = []}) {
+  const classes = tags.join(' ');
+  return `<div class="custom-card ${classes} card p-0 mb-4 me-sm-4 rounded-0 shadow-sm" style="width: 18rem;">
+  <img src="${photo}" class="card-img-top rounded-0" alt="${model}">
+  <div class="tag-new d-flex justify-content-center align-items-center">New</div>
+  <div class="custom-card__extra">
+    <span class="custom-card__extra-text">SOLD</span>
+  </div>
+  <div></div>
+  <div class="card-body pb-4">
+    <div class="d-flex mb-2">
+      <span class="custom-card__year me-2">${year}</span>
+      <h5 class="card-title custom-card__title">${model} ${modelDetails}</h5>
+    </div>
+    <div class="d-flex align-items-center">
+      <span class="d-block custom-card__description mr-2">${race} km</span>
+      <span class="d-block custom-card__description mr-2">${transmission}</span>
+      <span class="d-block custom-card__description mr-2">${engine}</span>
+    </div>
+    <span class="custom-card__price d-block mt-4">$${price}</span>
+  </div>
+</div>`
+}
+
+const yearSelect = document.querySelector('#year-select');
+const modelSelect = document.querySelector('#model-select');
+
+function getFilteredCars() {
+  let filtered = [...cars];
+  
+  if(yearSelect.value) {
+    filtered = filtered.filter(({year}) => year === +yearSelect.value);
+  }
+
+  if(modelSelect.value) {
+    filtered = filtered.filter(({model}) => model === modelSelect.value);
+  }
+
+  return filtered;
+}
+
+function fillSelect(select, field) {
+  const set = new Set();
+  cars.forEach((car) => {
+    set.add(car[field]);
+  });
+
+  set.forEach((value) => {
+    select.innerHTML += `<option value="${value}">${value}</option>`;
+  });
+}
+
+fillSelect(yearSelect, 'year');
+fillSelect(modelSelect, 'model');
+
+function renderCards() {
+  const carsList = document.querySelector('#cars-list');
+  carsList.innerHTML = '';
+  getFilteredCars().forEach((car) => carsList.innerHTML += getCard(car));
+}
+
+renderCards();
+
+yearSelect.addEventListener('change', renderCards);
+modelSelect.addEventListener('change', renderCards);
+
+// Validating email and changing input border color and showing icon  
+
+const email = document.querySelector('#email');
+const iconSuccess = document.querySelector('.subscription__icon-success');
+const iconError = document.querySelector('.subscription__icon-error');
+const errorText = document.querySelector('.subscription__error-text');
+const btnSubscribe = document.querySelector('.btn--subscribe');
+
+let regExp = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+function check() {
+  if(email.value.match(regExp)) {
+    email.style.borderColor = '#507a36';
+    iconError.style.display = 'none';
+    iconSuccess.style.display = 'block';
+    errorText.style.display = 'none';
+  } else {
+    email.style.borderColor = '#ff2307';
+    iconError.style.display = 'block';
+    iconSuccess.style.display = 'none';
+    errorText.style.display = 'block';
+  }
+}
+
+email.addEventListener('keyup', check);
